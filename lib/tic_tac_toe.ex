66e4648227aee68fbc309b2e5a952bd64d5fa301
Jonathan Manahan <jonathanmanahan@Jonathans-MacBook-Pro.local>
2022-03-10 14:22:56 -0500

    Add welcome message

--- lib/tic_tac_toe.ex
+++ lib/tic_tac_toe.ex
@@ -2,4 +2,9 @@ defmodule TicTacToe do
   @moduledoc """
   Documentation for `TicTacToe`.
   """
+
+  @spec welcome_message :: :ok
+  def welcome_message do
+    CommandLine.display("Welcome to Tic-Tac-Toe")
+  end
 end
---
+++ test/command_line_test.exs
@@ -0,0 +1,9 @@
+defmodule CommandLineTest do
+  use ExUnit.Case
+  import ExUnit.CaptureIO
+
+  test "displays message" do
+    message = "foo"
+    assert capture_io(fn -> CommandLine.display(message) end) === message
+  end
+end
