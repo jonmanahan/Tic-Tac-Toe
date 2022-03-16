751a760dac75fbc67851ca140154985ec3a12add
Jonathan Manahan <jonathanmanahan@Jonathans-MacBook-Pro.local>
2022-03-16 14:25:21 -0400

    Fix board and add moduledoc to command_line_mock and display behaviour
    Fix display_empty_board to actually display entire empty board.
    Add the moduledoc to these files to satisfy credo.

--- lib/tic_tac_toe.ex
+++ lib/tic_tac_toe.ex
@@ -1,15 +1,20 @@
 defmodule TicTacToe do
   @moduledoc """
-  The module that handles the logic and communcation for a Command Line Interface
-  Tic-Tac-Toe game
+  The module that handles the logic and communication for a Tic-Tac-Toe game
   """
 
   @welcome_message "Welcome to Tic-Tac-Toe"
-  @display_empty_board "1 | 2 | 3"
+  @display_empty_board """
+  1 | 2 | 3
+  --|---|--
+  4 | 5 | 6
+  --|---|--
+  7 | 8 | 9
+  """
 
   @spec start(any()) :: :ok
   def start(writer) do
-    welcome = @welcome_message <> "\n\n" <> @display_empty_board <> "\n"
+    welcome = @welcome_message <> "\n\n" <> @display_empty_board
 
     writer.display(welcome)
   end
