5da2b2e73b229caea9207928a2dac2db08ec5889
Jonathan Manahan <jonathanmanahan@Jonathans-MacBook-Pro.local>
2022-03-17 11:56:57 -0400

    Create a mock dir in the test dir for the mocks to reside in
    Update `test_helpers` to grab each file in the mocks dir and
    run `Code.require_file` on each so the mocks can be organized
    and used in the tests.

--- test/command_line_test.exs
+++ test/command_line_test.exs
@@ -2,7 +2,7 @@ defmodule CommandLineTest do
   use ExUnit.Case
   import ExUnit.CaptureIO
 
-  test "(unit test) displays message" do
+  test "displays message" do
     message = "foo"
     assert capture_io(fn -> CommandLine.display(message) end) === message
   end
