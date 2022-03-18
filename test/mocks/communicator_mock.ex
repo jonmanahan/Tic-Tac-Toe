4ac4b9b327454c842917b85646ba8bc25b73c078
Jonathan Manahan <jonathanmanahan@Jonathans-MacBook-Pro.local>
2022-03-18 14:55:57 -0400

    Change the way the mock works to stop the process properly
    Use GenServer instead of Agent in the mock. Also supervise the
    CommunicatorMock to make sure the process is exited before the
    start of the next test.

--- test/mocks/communicator_mock.ex
+++ test/mocks/communicator_mock.ex
@@ -3,27 +3,39 @@ defmodule CommunicatorMock do
   The module that mocks the Command Line display function,
   returning the message instead of displaying it to the Command Line
   """
-  use Agent
+  use GenServer
 
   @behaviour CommunicationBehaviour
 
+  @impl true
   def display(message) do
-    start_mock_agent()
-    Agent.update(CommunicatorMockAgent, &(&1 <> message <> "\n"))
-    Agent.get(CommunicatorMockAgent, & &1)
+    GenServer.call(CommunicatorMockServer, {:display, message})
   end
 
+  @impl true
   def read_input() do
-    start_mock_agent()
-    user_input = "1"
-    Agent.update(CommunicatorMockAgent, &(&1 <>
-      "Please input desired placement: #{user_input}"))
-    Agent.get(CommunicatorMockAgent, & &1)
+    GenServer.call(CommunicatorMockServer, :read_input)
   end
 
-  defp start_mock_agent do
-    if !Process.whereis(CommunicatorMockAgent) do
-      Agent.start_link(fn -> "" end, name: CommunicatorMockAgent)
-    end
+  def start_link(_arg) do
+    GenServer.start_link(__MODULE__, "", name: CommunicatorMockServer)
+  end
+
+  @impl true
+  def init(full_message) do
+    {:ok, full_message}
+  end
+
+  @impl true
+  def handle_call({:display, message}, _from, full_message) do
+    full_message = full_message <> message
+    {:reply, full_message, full_message}
+  end
+
+  @impl true
+  def handle_call(:read_input, _from, full_message) do
+    user_input = "1"
+    full_message = full_message <> "Please input desired placement: #{user_input}"
+    {:reply, full_message, full_message}
   end
 end
