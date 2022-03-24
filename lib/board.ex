8aa9efad5178f1627799665dcf7b9f9362ba52f4
Jonathan Manahan <jonathanmanahan@Jonathans-MacBook-Pro.local>
2022-03-24 12:45:22 -0400

    Board determines if there is a winner, if so displays prompt and exits

--- lib/board.ex
+++ lib/board.ex
@@ -12,4 +12,53 @@ defmodule Board do
   def place_a_symbol(board, player_move, symbol) do
     Map.replace(board, String.to_integer(player_move), symbol)
   end
+
+  @spec has_player_won(map()) :: boolean()
+  def has_player_won(board) do
+    Enum.any?([get_all_rows_status(board), get_all_columns_status(board), get_both_diagonals_status(board)])
+  end
+
+  @spec get_board_dimensions(map()) :: integer()
+  def get_board_dimensions(board) do
+    trunc(:math.sqrt(Enum.count(board)))
+  end
+
+  @spec get_all_rows_status(map()) :: boolean()
+  defp get_all_rows_status(board) do
+    board_dimensions = get_board_dimensions(board)
+    board
+    |> Map.values()
+    |> Enum.chunk_every(board_dimensions)
+    |> Enum.map(fn rows -> Enum.uniq(rows) end)
+    |> Enum.any?(fn win -> Enum.count(win) == 1 end)
+  end
+
+  @spec get_all_columns_status(map()) :: boolean()
+  defp get_all_columns_status(board) do
+    board_dimensions = get_board_dimensions(board)
+    board
+    |> Map.values()
+    |> Enum.chunk_every(board_dimensions)
+    |> Enum.zip()
+    |> Enum.map(fn columns -> Enum.uniq(Tuple.to_list(columns)) end)
+    |> Enum.any?(fn win -> Enum.count(win) == 1 end)
+  end
+
+  @spec get_both_diagonals_status(map()) :: boolean()
+  defp get_both_diagonals_status(board) do
+    board_dimensions = get_board_dimensions(board)
+    board
+    |> Map.values()
+    |> Enum.chunk_every(board_dimensions)
+    |> get_diagonal_values(board_dimensions)
+    |> Enum.map(fn columns -> Enum.uniq(columns) end)
+    |> Enum.any?(fn win -> Enum.count(win) == 1 end)
+  end
+
+  @spec get_diagonal_values(list(), integer()) :: list()
+  defp get_diagonal_values(board, board_dimensions) do
+    forward_diagonal = for diagonal_index <- 0..board_dimensions - 1, do: Enum.at(Enum.at(board, diagonal_index), diagonal_index)
+    backward_diagonal = for diagonal_index <- 0..board_dimensions - 1, do: Enum.at(Enum.at(board, diagonal_index), (board_dimensions - 1) - diagonal_index)
+    [forward_diagonal, backward_diagonal]
+  end
 end
