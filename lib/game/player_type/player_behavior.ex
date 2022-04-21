33e5a87805bffeb0a00ccc5a05b33a790420ddad
jonmanahan <jmanahan@8thlight.com>
2022-04-21 18:01:58 -0400

    Add missing alias to player behaviour

--- lib/game/player_type/player_behavior.ex
+++ lib/game/player_type/player_behavior.ex
@@ -2,5 +2,8 @@ defmodule Game.PlayerType.PlayerBehaviour do
   @moduledoc """
   The module that defines a generic players functions
   """
+
+  alias Game.Player
+
   @callback valid_input(player :: Player.t(), board :: map(), communicator :: any()) :: tuple()
 end
