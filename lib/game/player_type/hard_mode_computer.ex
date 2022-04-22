8f100de87b74fdef4bfe41cf6c01b9b066cabd21
jonmanahan <jmanahan@8thlight.com>
2022-04-22 14:33:15 -0400

    Add specs to hard_mode_computer functions

--- lib/game/player_type/hard_mode_computer.ex
+++ lib/game/player_type/hard_mode_computer.ex
@@ -13,7 +13,7 @@ defmodule Game.PlayerType.HardComputerPlayer do
 
   @impl PlayerBehaviour
   def valid_input(%Player{symbol: symbol}, board, communicator) do
-    moves_with_scores = get_available_moves_with_scores(board, symbol)
+    moves_with_scores = get_available_moves_with_score(board, symbol)
 
     valid_move = get_best_move(Map.keys(moves_with_scores), Map.values(moves_with_scores), symbol)
 
@@ -22,14 +22,15 @@ defmodule Game.PlayerType.HardComputerPlayer do
     {:ok, valid_move}
   end
 
-
-  defp get_available_moves_with_scores(@empty_board, _symbol), do: %{1 => 10}
-  defp get_available_moves_with_scores(board, symbol) do
+  @spec get_available_moves_with_score(map(), String.t()) :: map()
+  defp get_available_moves_with_score(@empty_board, _symbol), do: %{1 => 10}
+  defp get_available_moves_with_score(board, symbol) do
     board
     |> Board.available_spaces()
     |> Map.new(fn {position, _empty_space} -> {position, minimax(Board.place_a_symbol(board, position, symbol), 0, symbol)} end)
   end
 
+  @spec minimax(map(), non_neg_integer(), String.t()) :: integer()
   defp minimax(board, depth, symbol) do
     game_status = Board.game_status(board)
 
@@ -43,6 +44,7 @@ defmodule Game.PlayerType.HardComputerPlayer do
     end
   end
 
+  @spec get_best_move(list(), list(), String.t()) :: {:ok, non_neg_integer()}
   defp get_best_move(moves, scores, symbol) do
     case symbol do
       "X" -> Enum.at(moves, Enum.find_index(scores, fn score -> score == Enum.max(scores) end))
@@ -50,10 +52,12 @@ defmodule Game.PlayerType.HardComputerPlayer do
     end
   end
 
+  @spec calculate_moves_score(non_neg_integer(), :tie | :won, String.t()) :: integer()
   defp calculate_moves_score(_depth, :tied, _symbol), do: 0
   defp calculate_moves_score(depth, :won, "X"), do: 10 - depth
   defp calculate_moves_score(depth, :won, "O"), do: -10 + depth
 
+  @spec get_scores_for_all_potential_moves(map(), non_neg_integer(), String.t()) :: list()
   defp get_scores_for_all_potential_moves(board, depth, symbol) do
     board
     |> Board.available_spaces()
