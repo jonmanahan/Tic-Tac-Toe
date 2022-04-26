0c32793ca21571b01f3aabae837ef865cbea0801
jonmanahan <jmanahan@8thlight.com>
2022-04-26 14:32:31 -0400

    Fix Typo

--- test/game/game_setup_test.exs
+++ test/game/game_setup_test.exs
@@ -12,7 +12,7 @@ defmodule GameSetupTest do
   @mock_command_line %CommandLine{communicator: CommunicatorMock}
 
   describe "setup_players/1" do
-    test "returns players containing a Human player 1 and Human player 2 with their assosciated symbols" do
+    test "returns players containing a Human player 1 and Human player 2 with their associated symbols" do
       user_inputs = ["1", "1"]
       start_supervised!({@mock_command_line.communicator, user_inputs})
 
@@ -20,7 +20,7 @@ defmodule GameSetupTest do
         %Players{player_one: %Player{type: HumanPlayer, symbol: "X"}, player_two: %Player{type: HumanPlayer, symbol: "O"}}
     end
 
-    test "returns players containing a Human player 1 and an Easy Computer player 2 with their assosciated symbols" do
+    test "returns players containing a Human player 1 and an Easy Computer player 2 with their associated symbols" do
       user_inputs = ["1", "2", "1"]
       start_supervised!({@mock_command_line.communicator, user_inputs})
 
@@ -28,7 +28,7 @@ defmodule GameSetupTest do
         %Players{player_one: %Player{type: HumanPlayer, symbol: "X"}, player_two: %Player{type: EasyComputerPlayer, symbol: "O"}}
     end
 
-    test "returns players containing a Human player 1 and an Unbeatable Computer player 2 with their assosciated symbols" do
+    test "returns players containing a Human player 1 and an Unbeatable Computer player 2 with their associated symbols" do
       user_inputs = ["1", "2", "2"]
       start_supervised!({@mock_command_line.communicator, user_inputs})
 
@@ -36,7 +36,7 @@ defmodule GameSetupTest do
         %Players{player_one: %Player{type: HumanPlayer, symbol: "X"}, player_two: %Player{type: HardComputerPlayer, symbol: "O"}}
     end
 
-    test "returns players containing an Easy Computer player 1 and Human player 2 with their assosciated symbols" do
+    test "returns players containing an Easy Computer player 1 and Human player 2 with their associated symbols" do
       user_inputs = ["2", "1", "1"]
       start_supervised!({@mock_command_line.communicator, user_inputs})
 
@@ -44,7 +44,7 @@ defmodule GameSetupTest do
         %Players{player_one: %Player{type: EasyComputerPlayer, symbol: "X"}, player_two: %Player{type: HumanPlayer, symbol: "O"}}
     end
 
-    test "returns players containing an Unbeatable Computer player 1 and Human player 2 with their assosciated symbols" do
+    test "returns players containing an Unbeatable Computer player 1 and Human player 2 with their associated symbols" do
       user_inputs = ["2", "2", "1"]
       start_supervised!({@mock_command_line.communicator, user_inputs})
 
@@ -52,7 +52,7 @@ defmodule GameSetupTest do
         %Players{player_one: %Player{type: HardComputerPlayer, symbol: "X"}, player_two: %Player{type: HumanPlayer, symbol: "O"}}
     end
 
-    test "returns players containing an Easy Computer player 1 and an Easy Computer player 2 with their assosciated symbols" do
+    test "returns players containing an Easy Computer player 1 and an Easy Computer player 2 with their associated symbols" do
       user_inputs = ["2", "1", "2", "1"]
       start_supervised!({@mock_command_line.communicator, user_inputs})
 
@@ -60,7 +60,7 @@ defmodule GameSetupTest do
         %Players{player_one: %Player{type: EasyComputerPlayer, symbol: "X"}, player_two: %Player{type: EasyComputerPlayer, symbol: "O"}}
     end
 
-    test "returns players containing an Easy Computer player 1 and an Unbeatable Computer player 2 with their assosciated symbols" do
+    test "returns players containing an Easy Computer player 1 and an Unbeatable Computer player 2 with their associated symbols" do
       user_inputs = ["2", "1", "2", "2"]
       start_supervised!({@mock_command_line.communicator, user_inputs})
 
@@ -68,7 +68,7 @@ defmodule GameSetupTest do
         %Players{player_one: %Player{type: EasyComputerPlayer, symbol: "X"}, player_two: %Player{type: HardComputerPlayer, symbol: "O"}}
     end
 
-    test "returns players containing an Unbeatable Computer player 1 and an Easy Computer player 2 with their assosciated symbols" do
+    test "returns players containing an Unbeatable Computer player 1 and an Easy Computer player 2 with their associated symbols" do
       user_inputs = ["2", "2", "2", "1"]
       start_supervised!({@mock_command_line.communicator, user_inputs})
 
@@ -76,7 +76,7 @@ defmodule GameSetupTest do
         %Players{player_one: %Player{type: HardComputerPlayer, symbol: "X"}, player_two: %Player{type: EasyComputerPlayer, symbol: "O"}}
     end
 
-    test "returns players containing an Unbeatable Computer player 1 and an Unbeatable Computer player 2 with their assosciated symbols" do
+    test "returns players containing an Unbeatable Computer player 1 and an Unbeatable Computer player 2 with their associated symbols" do
       user_inputs = ["2", "2", "2", "2"]
       start_supervised!({@mock_command_line.communicator, user_inputs})
 
