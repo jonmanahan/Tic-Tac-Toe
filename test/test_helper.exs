aaf34c11a0cf73914621d22ee91d06f294bd57be
jonmanahan <jmanahan@8thlight.com>
2022-04-28 13:34:16 -0400

    Create a test helper struct to make setup inputs more legible

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -4,3 +4,8 @@ ExUnit.start()
 |> Path.join("*.ex")
 |> Path.wildcard()
 |> Enum.map(&Code.require_file/1)
+
+"./test/test_helpers/"
+|> Path.join("*.ex")
+|> Path.wildcard()
+|> Enum.map(&Code.require_file/1)
---
+++ test/test_helpers/test_helper.ex
@@ -0,0 +1,13 @@
+defmodule TestHelper do
+  @moduledoc """
+  The module that descriptively defines the player selection and difficulty selection inputs
+  """
+
+  defstruct [
+    human_player: "1",
+    computer_player: "2",
+    easy_mode_computer: "1",
+    hard_mode_computer: "2",
+    invalid_input: "5"
+  ]
+end
