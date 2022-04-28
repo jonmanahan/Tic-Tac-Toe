efd217ec0b333fe45e54ff86cc6b54097f712043
jonmanahan <jmanahan@8thlight.com>
2022-04-28 17:08:01 -0400

    Player and difficulty selection formatters to one generic formatter

--- lib/game/game_setup.ex
+++ lib/game/game_setup.ex
@@ -15,53 +15,55 @@ defmodule Game.GameSetup do
 
   @computer_types [%{type: EasyComputerPlayer, name: "Easy"}, %{type: HardComputerPlayer, name: "Unbeatable"}]
   @player_types [%{type: HumanPlayer, name: "Human"}, %{type: @computer_types, name: "Computer"}]
-  @symbols ["X", "O"]
 
   @spec setup_players(any()) :: Players.t()
   def setup_players(user_interface) do
-    [player_one, player_two] = Enum.map(@symbols, &(create_player(user_interface, &1)))
+    symbols = [{1, "X"}, {2, "O"}]
+    [player_one, player_two] = Enum.map(symbols, &(create_player(user_interface, &1)))
 
     Players.set_players(player_one, player_two)
   end
 
-  @spec select_player_type(String.t(), any()) :: player
-  defp select_player_type(player_symbol, user_interface) do
-    player_symbol
-    |> user_interface.formatter.format_player_setup(@player_types, @symbols)
+  @spec select_player_type(list(), String.t(), any()) :: player
+  defp select_player_type(player_types, prompt, user_interface) do
+    player_types
+    |> user_interface.formatter.format_setup_prompt(prompt)
     |> user_interface.communicator.read_input()
     |> Validator.validate_setup(@player_types)
-    |> get_valid_player_type(player_symbol, user_interface)
+    |> get_valid_player_type(prompt, user_interface)
   end
 
   @spec get_valid_player_type({:ok, list() | player} | {:invalid, atom()}, String.t(), any()) :: list() | player
-  defp get_valid_player_type({:ok, player_type}, _player_symbol, _user_interface), do: player_type
-  defp get_valid_player_type({:invalid, _reason}, player_symbol, user_interface) do
+  defp get_valid_player_type({:ok, player_type}, _prompt, _user_interface), do: player_type
+  defp get_valid_player_type({:invalid, _reason}, prompt, user_interface) do
     user_interface.communicator.display(Message.invalid_setup_input())
-    select_player_type(player_symbol, user_interface)
+    select_player_type(@player_types, prompt, user_interface)
   end
 
-  @spec select_difficulty(player | list(), any()) :: player
-  defp select_difficulty(HumanPlayer, _user_interface), do: HumanPlayer
-  defp select_difficulty(computer_types, user_interface) do
+  @spec select_difficulty(player | list(), String.t(), any()) :: player
+  defp select_difficulty(HumanPlayer, _prompt, _user_interface), do: HumanPlayer
+  defp select_difficulty(computer_types, prompt, user_interface) do
     computer_types
-    |> user_interface.formatter.format_computer_difficulty_setup()
+    |> user_interface.formatter.format_setup_prompt(prompt)
     |> user_interface.communicator.read_input()
     |> Validator.validate_setup(@computer_types)
-    |> get_valid_difficulty(user_interface)
+    |> get_valid_difficulty(prompt, user_interface)
   end
 
-  @spec get_valid_difficulty({:ok, player} | {:invalid, atom()}, any()) :: player
-  defp get_valid_difficulty({:ok, difficulty}, _user_interface), do: difficulty
-  defp get_valid_difficulty({:invalid, _reason}, user_interface) do
+  @spec get_valid_difficulty({:ok, player} | {:invalid, atom()}, String.t(), any()) :: player
+  defp get_valid_difficulty({:ok, difficulty}, _prompt, _user_interface), do: difficulty
+  defp get_valid_difficulty({:invalid, _reason}, prompt, user_interface) do
     user_interface.communicator.display(Message.invalid_setup_input())
-    select_difficulty(@computer_types, user_interface)
+    select_difficulty(@computer_types, prompt, user_interface)
   end
 
-  @spec create_player(any(), String.t()) :: Player.t()
-  defp create_player(user_interface, player_symbol) do
-    player_symbol
-    |> select_player_type(user_interface)
-    |> select_difficulty(user_interface)
+  @spec create_player(any(), tuple()) :: Player.t()
+  defp create_player(user_interface, {player_number, player_symbol}) do
+    player_select_prompt = "Please select Player #{player_number} (#{player_symbol}) => "
+    difficulty_select_prompt = "Please select Difficulty => "
+    @player_types
+    |> select_player_type(player_select_prompt, user_interface)
+    |> select_difficulty(difficulty_select_prompt, user_interface)
     |> Player.create_player(player_symbol)
   end
 end
